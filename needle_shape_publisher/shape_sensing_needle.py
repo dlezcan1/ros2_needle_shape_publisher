4aff7caa101f58f1fe096216098c82e257585bab
Dimitri Lezcano <dlezcan1@jh.edu>
2021-11-11 16:21:34 -0500

    Added curvature processing. Publish rate throttled
    Currently, the publishing/optimization of the shape takes
    very long and causes for all of the bandwidth to be taken
    up by the shape publisher. Curvature publisher is fast.
    May have to decompose shape-sensing node into a composable
    node.

--- needle_shape_publisher/shape_sensing_needle.py
+++ needle_shape_publisher/shape_sensing_needle.py
@@ -7,8 +7,8 @@ from rclpy.node import Node
 from rclpy.parameter import Parameter
 
 # messages
-from std_msgs.msg import Header, Float64MultiArray
-from geometry_msgs.msg import Pose, PoseArray
+from std_msgs.msg import Header, Float64MultiArray, MultiArrayDimension, MultiArrayLayout
+from geometry_msgs.msg import Point, Pose, PoseArray
 from rcl_interfaces.msg import ParameterDescriptor, SetParametersResult
 
 # services
@@ -22,6 +22,8 @@ from needle_shape_sensing.shape_sensing import ShapeSensingFBGNeedle
 class ShapeSensingNeedleNode( Node ):
     # PARAMETER NAMES
     PARAM_NEEDLE = "needle"
+
+    # - needle parameters
     PARAM_NEEDLEPARAMFILE = ".".join( [ PARAM_NEEDLE, "paramFile" ] )  # needle parameter json file
     PARAM_NEEDLESN = ".".join( [ PARAM_NEEDLE, "serialNumber" ] )  # needle serial number
     PARAM_NEEDLELENGTH = ".".join( [ PARAM_NEEDLE, "length" ] )  # needle length
@@ -29,10 +31,17 @@ class ShapeSensingNeedleNode( Node ):
     PARAM_AAS = ".".join( [ PARAM_NEEDLE, "activeAreas" ] )  # needle number of active areas
     PARAM_AAWEIGHTS = ".".join( [ PARAM_AAS, "weights" ] )  # needle AA reliability weightings
     PARAM_SLOCS = ".".join( [ PARAM_AAS, 'locations' ] )  # needle AA locations from tip of the needle
-    PARAM_NUMSIGNALS = ".".join( [ PARAM_NEEDLE, "sensor.numberSignals" ] )  # number of signals to gather for sampling
-    PARAM_KCINIT = ".".join( [ PARAM_NEEDLE, 'optimizer.initial_kappa_c' ] )  # initial kappa_c for optimization
-    PARAM_WINIT = ".".join( [ PARAM_NEEDLE, 'optimizer.initial_w_init' ] )  # initial omega_init for optimization
     PARAM_NEEDLESHAPE = ".".join( (PARAM_NEEDLE, "shape_type") )  # unused, needle shape type
+
+    # - FBG signal parameters
+    PARAM_NUMSIGNALS = ".".join( [ PARAM_NEEDLE, "sensor.numberSignals" ] )  # number of signals to gather for sampling
+
+    # - optimization options
+    PARAM_OPTIMIZER = ".".join( [ PARAM_NEEDLE, 'optimizer' ] )
+    PARAM_KCINIT = ".".join( [ PARAM_OPTIMIZER, 'initial_kappa_c' ] )  # initial kappa_c for optimization
+    PARAM_WINIT = ".".join( [ PARAM_OPTIMIZER, 'initial_w_init' ] )  # initial omega_init for optimization
+    PARAM_OPTIM_MAXITER = ".".join( [ PARAM_OPTIMIZER, 'max_iterations' ] )
+    PARAM_OPTIM_MAXITER_LB = 2
 
     def __init__( self, name="ShapeSensingNeedle" ):
         super().__init__( name )
@@ -40,21 +49,28 @@ class ShapeSensingNeedleNode( Node ):
         # declare and get parameters
         pd_needleparam = ParameterDescriptor( name=self.PARAM_NEEDLEPARAMFILE, type=Parameter.Type.STRING.value,
                                               description='needle parameter json file.', read_only=True )
-        pd_numsignals = ParameterDescriptor( name=self.PARAM_NUMSIGNALS, type=Parameter.Type.INTEGER.value )
+        needleparam_file = self.declare_parameter( pd_needleparam.name, descriptor=pd_needleparam ). \
+            get_parameter_value().string_value
+
+        pd_numsamples = ParameterDescriptor( name=self.PARAM_NUMSIGNALS, type=Parameter.Type.INTEGER.value )
+        self.num_samples = self.declare_parameter( pd_numsamples.name, descriptor=pd_numsamples,
+                                                   value=200 ).get_parameter_value().integer_value
+
         pd_kc = ParameterDescriptor( name=self.PARAM_KCINIT, type=Parameter.Type.DOUBLE_ARRAY.value,
                                      description="kappa_c initalization values" )
+        self.kc_i = self.declare_parameter( pd_kc.name, descriptor=pd_kc,
+                                            value=[ 0.002 ] ).get_parameter_value().double_array_value
+
         pd_winit = ParameterDescriptor( name=self.PARAM_WINIT, type=Parameter.Type.DOUBLE_ARRAY.value,
                                         description="omega_init initialization values (3D vector)" )
-
-        needleparam_file = self.declare_parameter( self.PARAM_NEEDLEPARAMFILE, descriptor=pd_needleparam ). \
-            get_parameter_value().string_value
-        self.num_samples = self.declare_parameter( self.PARAM_NUMSIGNALS, descriptor=pd_numsignals,
-                                                   value=200 ).get_parameter_value().integer_value
-        self.kc_i = self.declare_parameter( self.PARAM_KCINIT, descriptor=pd_kc,
-                                            value=[ 0.002 ] ).get_parameter_value().double_array_value
-        self.w_init_i = np.array( self.declare_parameter( self.PARAM_WINIT, descriptor=pd_winit,
+        self.w_init_i = np.array( self.declare_parameter( pd_winit.name, descriptor=pd_winit,
                                                           value=[ self.kc_i[ 0 ], 0.0, 0.0 ]
                                                           ).get_parameter_value().double_array_value )
+
+        pd_optim_maxiter = ParameterDescriptor( name=self.PARAM_OPTIM_MAXITER, type=Parameter.Type.INTEGER.value,
+                                                description="Maximum iterations for convergence" )
+        optim_maxiter = self.declare_parameter( pd_optim_maxiter.name, value=15,
+                                                descriptor=pd_optim_maxiter ).get_parameter_value().integer_value
 
         # get shape-sensing FBG Needle object
         try:
@@ -62,8 +78,9 @@ class ShapeSensingNeedleNode( Node ):
 
             # make all positive since we are using processed wavelengths
             self.ss_needle.ref_wavelengths = np.ones_like( self.ss_needle.ref_wavelengths )
-            self.ss_needle.current_depth = 130 # set the current depth
-
+            self.ss_needle.current_depth = 130  # set the current depth # TODO: needle pose subscriber
+            self.ss_needle.optimizer.options[ 'options' ] = { 'maxiter': optim_maxiter }
+            #
             # container of wavelengths
             self.__wavelength_container = np.zeros( (self.num_samples,
                                                      self.ss_needle.num_channels * self.ss_needle.num_activeAreas) )
@@ -110,20 +127,27 @@ class ShapeSensingNeedleNode( Node ):
         self.declare_parameter( self.PARAM_AAWEIGHTS, descriptor=pd_aawgts, value=w )
 
         # create publishers
-        self.pub_shape = self.create_publisher( PoseArray, 'state/shape', 10 )
-        self.pub_kc = self.create_publisher( Float64MultiArray, 'state/kappac', 10 )
-        self.pub_winit = self.create_publisher( Float64MultiArray, 'state/winit', 10 )
+        self.pub_shape = self.create_publisher( PoseArray, 'state/shape', 1 )
+        self.pub_curvatures = self.create_publisher( Float64MultiArray, 'state/curvatures', 10 )
+        self.pub_kc = self.create_publisher( Float64MultiArray, 'state/kappac', 1 )
+        self.pub_winit = self.create_publisher( Float64MultiArray, 'state/winit', 1 )
 
         # create subscriptions
-        self.sub_signals = self.create_subscription( Float64MultiArray, 'sensor/processed', self.signals_subcallback,
+        self.sub_signals = self.create_subscription( Float64MultiArray, 'sensor/processed', self.sub_signals_callback,
                                                      10 )
+        self.sub_entrypoint = self.create_subscription( Point, 'state/skin_entry', self.sub_entrypoint_callback,
+                                                        10 )
+        self.sub_needlepose = self.create_subscription( Pose, 'state/pose', self.sub_needlepose_callback,
+                                                        10 )
 
         # create services
         # TODO: update needle shape type service
 
         # create timers
-        timer_period = 0.05  # seconds
-        self.pub_shape_timer = self.create_timer( timer_period, self.publish_shape )
+        shape_timer_period = 3 # seconds
+        curv_timer_period = 0.01  # seconds
+        self.pub_shape_timer = self.create_timer( shape_timer_period, self.publish_shape )
+        self.pub_curvatures_timer = self.create_timer( curv_timer_period, self.publish_curvatures )
 
         # set parameter callback function
         self.add_on_set_parameters_callback( self.parameters_callback )
@@ -178,6 +202,7 @@ class ShapeSensingNeedleNode( Node ):
                     self.update_numsamples( num_samples )
                     self.get_logger().info( f"Updated number of signals to {num_samples}." )
 
+                # try
                 except ValueError as e:
                     successful = False
                     reasons.append( f"{self.PARAM_NUMSIGNALS} must be > 0" + '\n' + str( e ) )
@@ -185,23 +210,47 @@ class ShapeSensingNeedleNode( Node ):
 
                 # except
             # elif: parameter: NUMSIGNALS
+
+            elif param.name == self.PARAM_OPTIM_MAXITER:
+                optim_maxiter = param.get_parameter_value().integer_value
+                if optim_maxiter < self.PARAM_OPTIM_MAXITER_LB:
+                    successful = False
+                    reasons.append( f"{self.PARAM_OPTIM_MAXITER} must be >= {self.PARAM_OPTIM_MAXITER_LB}" )
+
+                # if
+                self.ss_needle.optimizer.options[ 'options' ] = { 'maxiter': optim_maxiter }
+
+            # elif
         # for
 
         return SetParametersResult( successful=successful, reason="\n".join( reasons ) )
 
     # parameters_callback
 
+    def publish_curvatures( self ):
+        """ Publish the curvatures of the shape-sensing needle"""
+        # current_curvatures are N x 2 ( columns are: x,  y ) -> ravel('F') -> (X_AA1, X_AA2, ..., Y_AA1, Y_AA2,...)
+        itemsize = self.ss_needle.current_curvatures.dtype.itemsize
+        dimx = MultiArrayDimension( label="x", stride=itemsize,
+                                    size=self.ss_needle.current_curvatures.shape[ 0 ] * itemsize )
+        dimy = MultiArrayDimension( label="y", stride=itemsize,
+                                    size=self.ss_needle.current_curvatures.shape[ 0 ] * itemsize )
+
+        msg = Float64MultiArray( data=self.ss_needle.current_curvatures.ravel( order='F' ).tolist(),
+                                 layout=MultiArrayLayout( dim=[ dimx, dimy ] ) )
+
+        self.pub_curvatures.publish(msg)
+
+    # publish_curvatures
+
     def publish_shape( self ):
         """ Publish the 3D needle shape"""
         if self.__wavelength_container_full:
             # get needle shape
-            self.get_logger().info(f"w_init_i shape: {self.w_init_i.shape}")
             if self.ss_needle.current_shapetype & NEEDLESHAPETYPE.SINGLEBEND_SINGLELAYER == NEEDLESHAPETYPE.SINGLEBEND_SINGLELAYER:  # single layer
                 pmat, Rmat = self.ss_needle.get_needle_shape( self.kc_i[ 0 ], self.w_init_i )
-                self.get_logger().info("SINGLELAYER insertion")
 
             elif self.ss_needle.current_shapetype & NEEDLESHAPETYPE.SINGLEBEND_DOUBLELAYER == 0x02:  # 2 layers
-                self.get_logger().info( "SINGLEBEND_DOUBLELAYER insertion" )
                 if len( self.kc_i ) < 2:
                     pmat, Rmat = self.ss_needle.get_needle_shape( self.kc_i[ 0 ], self.kc_i[ 0 ], self.w_init_i )
                 else:
@@ -222,9 +271,9 @@ class ShapeSensingNeedleNode( Node ):
 
             # check to make sure messages are not None
             if pmat is None or Rmat is None:
-                self.get_logger().warn(f"pmat or Rmat is None: {pmat}, {Rmat}")
-                self.get_logger().warn(f"Current shapetype: {self.ss_needle.current_shapetype}")
-                assert(self.ss_needle.current_shapetype == NEEDLESHAPETYPE.SINGLEBEND_SINGLELAYER)
+                self.get_logger().warn( f"pmat or Rmat is None: {pmat}, {Rmat}" )
+                self.get_logger().warn( f"Current shapetype: {self.ss_needle.current_shapetype}" )
+                assert (self.ss_needle.current_shapetype == NEEDLESHAPETYPE.SINGLEBEND_SINGLELAYER)
                 return
 
             # generate pose message
@@ -317,7 +366,21 @@ class ShapeSensingNeedleNode( Node ):
 
     # set_needleparameters
 
-    def signals_subcallback( self, msg: Float64MultiArray ):
+    def sub_entrypoint_callback( self, msg: Point ):
+        """ Call back to needle skin entry point"""
+        # TODO: subscription entrypoint
+        pass
+
+    # sub_entrypoint_callback
+
+    def sub_needlepose_callback( self, msg: Pose ):
+        """ Call back to needle base pose"""
+        # TODO: subscription needlepose
+        pass
+
+    # sub_needlepose_callback
+
+    def sub_signals_callback( self, msg: Float64MultiArray ):
         """ Call back to processed signals subscription"""
         # get the FBG signals
         # TODO: perform appending by channel
@@ -341,7 +404,7 @@ class ShapeSensingNeedleNode( Node ):
 
         # if
 
-    # signals_subcallback
+    # sub_signals_callback
 
     def update_numsamples( self, num_samples: int ):
         """ Update the number of FBG samples """
