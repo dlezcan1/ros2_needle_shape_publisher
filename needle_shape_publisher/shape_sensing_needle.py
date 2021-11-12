63be65154ea2b7b0abf8a63b0b18947e9aa2cf6e
Dimitri Lezcano <dlezcan1@jh.edu>
2021-11-12 13:25:01 -0500

    Added framework for multithreaded execution

---
+++ needle_shape_publisher/shape_sensing_needle.py
@@ -0,0 +1,40 @@
+import numpy as np
+from typing import List
+
+# ROS2 packages
+import rclpy
+from rclpy.node import Node
+from rclpy.parameter import Parameter
+
+# messages
+from std_msgs.msg import Header, Float64MultiArray, MultiArrayDimension, MultiArrayLayout
+from geometry_msgs.msg import Point, Pose, PoseArray
+from rcl_interfaces.msg import ParameterDescriptor, SetParametersResult
+
+# services
+
+# custom package
+from needle_shape_sensing import geometry
+from needle_shape_sensing.intrinsics import SHAPETYPE as NEEDLESHAPETYPE
+from needle_shape_sensing.shape_sensing import ShapeSensingFBGNeedle
+
+
+class ShapeSensingNeedleNode(Node):
+    """Needle to handle shape-sensing applications"""
+    # TODO
+    pass
+
+# class: SensorizedNeedleNode
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    # clean-up
+    rclpy.shutdown()
+
+# main
+
+if __name__ == "__main__":
+    main()
+
+# if __main__
--- setup.py
+++ setup.py
@@ -15,12 +15,14 @@ setup(
     zip_safe=True,
     maintainer='Dimitri Lezcano',
     maintainer_email='dlezcan1@jhu.edu',
-    description='Needle Shape Sensing Python node',
+    description='Needle Shape Sensing Python ROS 2 package',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'sensorized_shapesensing_needle = needle_shape_publisher.sensorized_shape_sensing_needle:main'
+                'sensorized_shapesensing_needle = needle_shape_publisher.sensorized_shape_sensing_needle:main',
+                'sensorized_needle = needle_shape_publisher.sensorized_needle:main',
+                'shapesensing_needle = needle_shape_publisher.shape_sensing_needle:main'
         ],
     },
 )
